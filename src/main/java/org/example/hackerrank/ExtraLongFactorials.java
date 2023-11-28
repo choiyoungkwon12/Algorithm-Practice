c0797e31a67a8b1ec0d1af8d345f155be35d0fda
choiyoungkwon12 <hpwh4457@naver.com>
2023-11-29 02:32:55 +0900

    Hackerrank ExtraLongFactorials

---
+++ src/main/java/org/example/hackerrank/ExtraLongFactorials.java
@@ -0,0 +1,45 @@
+package org.example.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class ExtraLongFactorials {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
+
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        // Write your code here
+        BigInteger bigInteger = new BigInteger("1");
+        for (int i = 2; i <= n ; i++) {
+            bigInteger = bigInteger.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(bigInteger);
+    }
+
+}
